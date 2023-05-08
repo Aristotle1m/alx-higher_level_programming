0edea696c5ce6f0005bea866a153ac1b213e2ee6
Chukwudi Egoebichi <chuksegoebichi.com>
2023-05-08 11:07:26 -0700

    header

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,17 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list docs
- * @n: integers
- * @next: points associated to the next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
-	    int n;
-	        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -20,3 +23,4 @@ void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
 #endif /* LISTS_H */
+
