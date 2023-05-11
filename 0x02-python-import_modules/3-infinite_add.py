c2e0ffbd1a78a4d40a5851e5397bac2d11a8508d
Chukwudi Egoebichi <chuksegoebichi.com>
2023-05-11 06:59:04 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def main():
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
     sum = 0
-
-    for count, args in enumerate(argv):
-        if count == 0:
-            continue
-
-        sum += int(args)
-
-    print(f"{sum:d}")
-
-
-if __name__ == '__main__':
-    main()
+    if num <= 1:
+        print(sum)
+    else:
+        for i in range(1, num):
+            sum += int(sys.argv[i])
+        print(sum)
