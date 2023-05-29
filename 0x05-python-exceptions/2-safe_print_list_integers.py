43cce09320fd4b78f553b5d3066a2a6580317136
Chukwudi Egoebichi <chuksegoebichi.com>
2023-05-29 11:51:02 +0000

    i am becoming aa fucking pro

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    count = 0
 
+
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end='')
-        except TypeError:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except (ValueError, NameError, TypeError):
             pass
-        except ValueError:
-            pass
-        else:
-            count += 1
 
     print()
-    return (count)
+    return j
