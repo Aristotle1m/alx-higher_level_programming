514d2d84553a8b06e0a12c85d3fc514db9ed02f5
Chukwudi Egoebichi <chuksegoebichi.com>
2023-06-05 16:16:41 +0000

    simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-
-This module is composed by a function that adds two numbers
-
+An empty rectangle class
 """
+
+
 class Rectangle:
-    """ Empty class """
+    """Rectangle class"""
     pass
